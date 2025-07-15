98eb398470da207798d1336c84076fa6ec051faf
Sheik Mohammed Ali M <mdali.sheik1613@gmail.com>
2025-07-15 18:43:32 +0530

    removed

--- Browser_agent/src/agent/deep_research/form_wrapper.py
+++
@@ -1,6 +0,0 @@
-import asyncio
-from formmanage import ask_user
-
-async def ask_user_async(prompt: str) -> str:
-    loop = asyncio.get_event_loop()
-    return await loop.run_in_executor(None, ask_user, prompt)
