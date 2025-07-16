cfb743d3cbfed65f36f8fb77753c0cad93dcace4
Vimalesh <vimalesh@ripplethrive.io>
2025-07-16 13:01:00 +0530

    RemovedUnwantedFiles

--- Browser_agent/google_api_test.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="Google_api_key")
-
-model = genai.GenerativeModel("gemini-2.0-flash")  # or "gemini-1.5-flash"
-response = model.generate_content("Hello Gemini")
-print(response.text)
